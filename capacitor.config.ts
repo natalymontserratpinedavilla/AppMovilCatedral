47e39ccf5776cc0c87583de216dc800b3c3059e1
natalymontserratpinedavilla <monzevilla1@gmail.com>
2025-11-25 01:22:54 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'APPMOVIL1',
+  webDir: 'www'
+};
+
+export default config;
